72922a5d73c1f004da5fc89e9d40957a087feba1
Jerome Marrero <jerome.marrero.perez@gmail.com>
2024-06-23 09:04:35 -0400

    double fetching error

--- src/DeckWrapper.jsx
+++ src/DeckWrapper.jsx
@@ -17,35 +17,36 @@ const DeckWrapper = () =>{
                 
             }
             catch(error){
-                console.error("error fetching back of card:", error);
+                console.error("error fetching back of deck:", error);
             }
         } 
-        getDeck();
-        deckRef.current = deck
-        
-    }, [])
+        getDeck()
+        console.log(deck)
+    }, []);
+        console.log(deck);
     
-    useEffect(function drawCardWhenClicked(){ 
-        async function getCard () { 
-            try{
-                const cardResponse = await axios({
-                method: "POST",
-                url:` https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
-                    })
-                console.log(cardResponse);
-            }
-            catch(error){
-                console.error("error fetching back of card:", error);
-            }
-        }
-    getCard()
-    }, [cards])
+    //drawingCard
+    // useEffect(() => {
+    //     async function getCard () { 
+    //         try{
+    //             const cardResponse = await axios({
+    //             method: "POST",
+    //             url:` https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
+    //                 })
+    //             console.log(cardResponse);
+    //         }
+    //         catch(error){
+    //             console.error("error fetching back of card:", error);
+    //         }
+    //     }
+    // getCard()
+    // }, [cards])
 
 
 
     return( 
     <div>
-      <img src='https://deckofcardsapi.com/static/img/back.png' alt="DeckOfCards" />
+      {deck ? <img src='https://deckofcardsapi.com/static/img/back.png' alt="DeckOfCards" />  : <h1> Loading ...</h1>}
       <button> DRAW CARD</button>
       <div></div>
     </div>
